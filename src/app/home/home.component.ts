e14950a22a87fb0bcc746e81804a5c029e95384b
Cong Ta <congtm20@gmail.com>
2021-11-01 09:53:31 +0700

    ng-Class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,20 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public name: string = 'Minh Công';
-  public age: number =18;
-  public fruits =['Apple','Orange','Melon','Strawberry','Mango','Cherry'];
-  constructor() { }
+  public age: number = 18;
+  public fruits = ['Apple', 'Orange', 'Melon', 'Strawberry', 'Mango', 'Cherry'];
+  public fruits2 = [
+    { fName: 'Apple', fPrice: -1 , Discount: false },
+    { fName: 'Orange', fPrice: 20, Discount: false },
+    { fName: 'Melon', fPrice: 15, Discount: false },
+    { fName: 'Strawberry', fPrice: 30, Discount: true},
+  ];
+  constructor() {}
 
   ngOnInit(): void {
-    console.log('Trai cay = ',this.fruits);
-
+    console.log('Trai cay = ', this.fruits2);
   }
   public resetName(): void {
     console.log('resetName');
-    this.age=18;
+    this.age = 18;
   }
 }
